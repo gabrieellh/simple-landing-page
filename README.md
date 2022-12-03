3c9a7326bd8645a400be844808956a04bddc9666
Gabriel Henrique <smartsbiel@gmail.com>
2022-12-03 20:37:40 -0300

    last-commit

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 <ul>
     <li>HTML</li>
     <li>CSS</li>
+    <li>JavaScript</li>
 </ul>
 
 ## 📢 Projeto
---
+++ scroll.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.top-of-site', {duration : 3000, origin: 'right'})
+sr.reveal('.main-content', {duration : 3000, origin: 'left'})
+sr.reveal('.projects', {duration : 3000,  origin: 'left'})
+sr.reveal('.redes', {duration : 3000,  origin: 'left'})
